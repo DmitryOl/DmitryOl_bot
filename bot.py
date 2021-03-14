c81bca2823cbc1d07d7814bfe23535bd992703b1
DmitryOl <wride@ya.ru>
2021-03-14 13:09:22 +0300

    добавил about

--- bot.py
+++ bot.py
@@ -10,11 +10,23 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.API_TOKEN)
 dp = Dispatcher(bot)
 
+# hendler na komandy /about
+@dp.message_handler(commands="about")
+async def cmd_test1(message: types.Message):
+    await message.reply("""
+    О боте, что я умею:
+        - вести учет расходов
+        - трекер дел
+        - записи
+    """)
+
 #  echo
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer(message.text)
 
-# run long polling
+
+
+# run bot
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
