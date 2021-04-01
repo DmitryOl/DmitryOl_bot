1cad2db319aaf0239fa2f51113ac9b4dcc941a19
DmitryOl <69640438+DmitryOl@users.noreply.github.com>
2021-04-01 23:31:57 +0300

    add music

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 #!/home/dmitry/DmitryOl_bot/venv/bin/python
 import config
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+#from aiogram import Bot, Dispatcher, executor, types
+from aiogram import *
 from SQLite_connect import SQLite_conn
 
 import platform
 import subprocess
 import re
 
+from pytube import YouTube
+import os
 
 # yroven log
 logging.basicConfig(level=logging.INFO)
@@ -28,7 +31,7 @@ elif platform.system() == "Linux":
 async def usr_db(message: types.Message):
     add_mes(message.chat.id, message.text)
 
-    all_user = db.user_exists() 
+    all_user = db.user_exists()
     await message.answer(all_user)
 
 
@@ -70,25 +73,57 @@ async def echo(message: types.Message):
         await message.answer(f"{message.text[3:]} :\n {per}")
     elif message.text[:3] == 'cmd' and config.M_C_ID != str(message.chat.id) :
         await message.answer(f"нет прав на запуск команды: {message.text[3:]}")
+    elif message.text[:2] in  ['yt', 'Yt'] and config.M_C_ID == str(message.chat.id) :
+        chat_id = message.chat.id
+        url = message.text[2:]
+        yt = YouTube(url)
+        if message.text.startswith == 'https://www.youtube.com/' or 'https://youtu.be/':
+            await bot.send_message(chat_id, f"Начинаю загрузку видео* : *{yt.title}*\n"
+            f"*С канала *: [{yt.author}]({yt.channel_url})")
+            await download_yt_video(url, message, bot)
+    elif message.text[:2] in ['ym', 'Ym'] and config.M_C_ID == str(message.chat.id):
+        chat_id = message.chat.id
+        url = message.text[2:]
+        yt = YouTube(url)
+        if message.text.startswith == 'https://www.youtube.com/' or 'https://youtu.be/':
+            await download_yt_music(url, message, bot)
     else:
         await message.answer(f"ваше сообщение: {message.text}")
 
 
+async def download_yt_video(url, message, bot):
+    yt = YouTube(url)
+    stream = yt.streams.filter(progressive=True, file_extension="mp4", res="360p")
+    stream.get_highest_resolution().download(f'{message.chat.id}', f'{message.chat.id}_{yt.title}')
+    with open(f"{message.chat.id}/{message.chat.id}_{yt.title}", 'rb') as video:
+        await bot.send_video(message.chat.id, video, caption=yt.title)
+        os.remove(f"{message.chat.id}/{message.chat.id}_{yt.title}")
+
+
+async def download_yt_music(url, message, bot):
+    yt = YouTube(url)
+    name = f"{yt.title}.mp3"
+    yt.streams.filter(only_audio=True).first().download(filename=name)
+    with open(f"{name}", 'rb') as audio:
+        await bot.send_audio(message.chat.id, audio, caption=f"{name}")
+        os.remove(f"{name}")
+
 
 #вынести проверку в отдельный файл
-def check_cmd(cmd=""):    
+def check_cmd(cmd=""):
     if cmd != '':
         return subprocess.check_output(cmd, shell = True).decode('utf-8')
     else:
         return f"запуск на {platform.system()} "
 
-def add_mes(user_id, user_mes):    
+def add_mes(user_id, user_mes):
     if (not db.user_find(user_id)):
         # если новый, добавляем в базу
         db.user_add(user_id)
 
     # распределяем сообщения по подходящим таблицам
     rslt = user_mes.split(' ')
+
     if rslt[0] == 'м' or rslt[0] == 'М': # если первое слово "м" то в мысли
         mes = re.sub('^\w{1}\s+', '', user_mes)
         db.add_mess_mind(user_id, mes)
@@ -109,7 +144,7 @@ def add_mes(user_id, user_mes):
             db.add_mess_action(user_id, note)
 
 
-    
+
 # run bot
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
